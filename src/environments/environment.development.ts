289199946347104a26c753e377552208ce94441a
veryfams <andres.meneses@ucuenca.edu.ec>
2026-04-14 17:26:09 -0500

    fix: cors + production env config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
-/*export const environment = {
-  baseUrl: 'http://192.168.1.13:3001/api',
-};
-*/
-
 export const environment = {
   baseUrl: 'http://localhost:3001/api',
 };
+
+/*
+export const environment = {
+  baseUrl: 'http://localhost:3001/api',
+};*/
